52058aecb49503e2852219dd96a4ad6508876132
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-02-19 06:41:31 +0000

    in the middle

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 root to:'homes#top'
+get "home/about"=>'homes#about'
 devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources:users, only:[:show, :edit]
-resources:books, only:[:new, :index, :edit]
+resources:books, only:[:new, :index, :edit,]
 end
